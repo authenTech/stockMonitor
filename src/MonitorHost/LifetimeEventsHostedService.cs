572c17b512aa55826df90c4921d31e11fb12ba42
AuthenTech Solutions <authenticjes@hotmail.com>
2021-09-08 20:31:15 -0500

    3rd commit

---
+++ src/MonitorHost/LifetimeEventsHostedService.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MonitorHost
+{
+    public class LifetimeEventsHostedService : IHostedService
+    {
+        private Timer _timer;
+        private int _invokeCount = 0;
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _timer = new Timer(CallEvery30Minutes, null, TimeSpan.FromSeconds(1), TimeSpan.FromMinutes(30));
+
+            return Task.CompletedTask;
+            // throw new NotImplementedException();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CallEvery30Minutes(object state)
+        {
+            _invokeCount++;
+                
+            if (_invokeCount == 3)
+            {
+                _timer.Change(Timeout.Infinite, Timeout.Infinite);
+            }
+        }
+    }
+}
---
+++ src/MonitorHost/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.IO;
+
+namespace MonitorHost
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureHostConfiguration(configHost =>
+                {
+                    configHost.SetBasePath(Directory.GetCurrentDirectory());
+                    configHost.AddJsonFile("hostsettings.json", optional: true);
+                    // configHost.AddEnvironmentVariables(prefix: "PREFIX_");
+                    configHost.AddCommandLine(args);
+                })
+                
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<LifetimeEventsHostedService>();
+                });
+        }
+    }
+}
